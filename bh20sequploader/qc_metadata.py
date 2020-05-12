f2e8b440abebc710c6e9f99044b5fc5560ba40bd
Maxat Kulmanov <coolmaksat@gmail.com>
2020-05-12 10:33:59 +0300

    .

--- bh20sequploader/qc_metadata.py
+++ bh20sequploader/qc_metadata.py
@@ -10,11 +10,11 @@ from pyshex.evaluate import evaluate
 
 def qc_metadata(metadatafile):
     schema_resource = pkg_resources.resource_stream(__name__, "bh20seq-schema.yml")
-    cache = {"https://raw.githubusercontent.com/arvados/bh20-seq-resource/master/bh20sequploader/bh20seq-schema.yml": schema_resource.read().decode("utf-8")}
+    cache = {"https://raw.githubusercontent.com/bio-ontology-research-group/bh20-seq-resource/master/bh20sequploader/bh20seq-schema.yml": schema_resource.read().decode("utf-8")}
     (document_loader,
      avsc_names,
      schema_metadata,
-     metaschema_loader) = schema_salad.schema.load_schema("https://raw.githubusercontent.com/arvados/bh20-seq-resource/master/bh20sequploader/bh20seq-schema.yml", cache=cache)
+     metaschema_loader) = schema_salad.schema.load_schema("https://raw.githubusercontent.com/bio-ontology-research-group/bh20-seq-resource/master/bh20sequploader/bh20seq-schema.yml", cache=cache)
 
     shex = pkg_resources.resource_stream(__name__, "bh20seq-shex.rdf").read().decode("utf-8")
 
@@ -25,7 +25,7 @@ def qc_metadata(metadatafile):
     try:
         doc, metadata = schema_salad.schema.load_and_validate(document_loader, avsc_names, metadatafile, True)
         g = schema_salad.jsonld_context.makerdf("workflow", doc, document_loader.ctx)
-        rslt, reason = evaluate(g, shex, doc["id"], "https://raw.githubusercontent.com/arvados/bh20-seq-resource/master/bh20sequploader/bh20seq-shex.rdf#submissionShape")
+        rslt, reason = evaluate(g, shex, doc["id"], "https://raw.githubusercontent.com/bio-ontology-research-group/bh20-seq-resource/master/bh20sequploader/bh20seq-shex.rdf#submissionShape")
 
         if not rslt:
             print(reason)
